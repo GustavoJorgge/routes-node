89d6196f737bed785c224cfc40fa37fc8dc0570f
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-03-24 22:24:22 -0300

    melhorando a busca com tolowercase

--- src/route.js
+++ src/route.js
@@ -8,7 +8,12 @@ export const routes = [{
     method: 'GET',
     path:  buildRoutePath('/users'),
     handler:(request,response)=>{
-        const users = database.select('users')
+
+        const {search} = request.query
+        const users = database.select('users', search ?{
+            name: search,
+            email: search,
+        }: null)
 
         console.log(request.query)
 
